1a6fadc4d3667cc2a900dc1be5844c2ef42b1c61
= <=>
2021-06-28 19:01:59 +0700

    perubahan

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@
 */
 
 $router->get('/', function () use ($router) {
-    return $router->app->version();
+    return "hello rasdi abdulrohman";
 });
 
 $router->group(['prefix'=>'api','middleware'=>'auth'],function() use($router) {
